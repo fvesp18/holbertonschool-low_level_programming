689299c74eae67c9fd4e746705dd4e7fc17f9922
fvesp18 <fespholberton@gmail.com>
2019-09-19 23:56:07 +0000

    REUSE SCRIPT FROM 2-print_alphabet.c, replace space with tabs

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 97; ch <= 122; ch++)
-        {
-                putchar(ch);
-        }
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
 	for (ch = 65; ch <= 90; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
