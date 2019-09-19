39edef4f8673ca244725500f11d3de4f1e0aab0a
fvesp18 <fespholberton@gmail.com>
2019-09-19 23:44:32 +0000

    Print lowercase alphabet and uppercase, end in line break.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - declare ch, run for loop to print alphabet, end new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        char ch;
+
+        for (ch = 97; ch <= 122; ch++)
+        {
+                putchar(ch);
+        }
+	for (ch = 65; ch <= 90; ch++)
+        {
+                putchar(ch);
+        }
+        putchar('\n');
+        return (0);
+}
