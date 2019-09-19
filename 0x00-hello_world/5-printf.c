ba6b44b41a78585a02e450803dc2e8a1342be049
fvesp18 <fespholberton@gmail.com>
2019-09-19 00:44:37 +0000

    addtion of commments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - printf function
+ * main - a function which will print quote
  *
- *
+ * Return: 0
  */
 
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
