007b69d4b68545c9ac4ea9e741be8f8a72e26f36
fvesp18 <fespholberton@gmail.com>
2019-09-19 01:40:39 +0000

    init commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-	sizeof(char);
-	sizeof(int);
-	sizeof(long_int);
-	sizeof(long_long_int);
-	sizeof(float);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
