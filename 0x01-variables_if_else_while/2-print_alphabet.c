f3edd9ad5a6c4c1928b95259e394d54206ba93e6
fvesp18 <fespholberton@gmail.com>
2019-09-19 18:58:29 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+        char ch;
+
+        for (ch = 97; ch <= 122; ch++)
+        {
+                putchar(ch);
+        }
+        putchar('\n');
+        return (0);
+}
