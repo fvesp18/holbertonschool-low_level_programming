cc860364db880048f9a7968318ee4b2285f870a5
fvesp18 <fespholberton@gmail.com>
2019-09-19 01:02:06 +0000

    print size of vars

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints various types of sizes
+ * Return: 0
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long_int);
+	sizeof(long_long_int);
+	sizeof(float);
+
+	return (0);
+}
