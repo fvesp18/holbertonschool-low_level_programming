1ca2dc5f06e77f5d56750d79cef010b852589086
fvesp18 <fespholberton@gmail.com>
2019-09-19 19:02:05 +0000

    Correct line spacing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
+ * main - declare ch, run for loop to print alphabet, end new line
  *
+ * Return: 0
  */
+
 int main(void)
 {
-        char ch;
-
-        for (ch = 97; ch <= 122; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+	char ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
