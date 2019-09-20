d19a379226c6b5d8e4fa854440bf9e56d8d1e636
fvesp18 <fespholberton@gmail.com>
2019-09-20 02:27:49 +0000

    init commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - for loop print 0-9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 121; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
