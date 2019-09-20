107941bdb493462ba5f76e7ef9aaad82d932d411
fvesp18 <fespholberton@gmail.com>
2019-09-20 00:36:26 +0000

    init commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - if q or e , dont print if a - z putchar
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	char ch;
+
+	for(ch = 97; ch <=122; ch++)
+		putchar(ch);
+	if (ch = 113 || ch = 101)
+		putchar!(ch);
+	return (0);
+}
