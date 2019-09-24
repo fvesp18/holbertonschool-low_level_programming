110cabb875b938141ad43821a661351a225ed8bf
fvesp18 <fespholberton@gmail.com>
2019-09-24 13:08:27 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - point of entry
+ * @n: parameter for return
+ * Return: 0, 1, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
