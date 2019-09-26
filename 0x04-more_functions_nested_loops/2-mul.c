dcd6b1d1652bbd1579316189ea56fcd1dcb01646
fvesp18 <fespholberton@gmail.com>
2019-09-26 13:54:51 +0000

    Return prod of multiple a and multiple b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - point of entry
+ * @a: first multiple
+ * @b: second multiple
+ * Return: prod
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
