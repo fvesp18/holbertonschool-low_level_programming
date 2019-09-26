96cec5fb05cf98ba9eafd91be27bf62b6288b87f
fvesp18 <fespholberton@gmail.com>
2019-09-26 19:08:03 +0000

    Loop 10 times int 0 - 14, add condition to print 10-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - point of entry
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int loop;
+	int numbers;
+
+	for (loop = 0; loop <= 9; loop++)
+	{
+		for(numbers = 0; numbers <= 14; numbers++)
+		{
+			if (numbers > 9)
+			{
+				_putchar((numbers / 10) + '0');
+			}
+				_putchar((numbers % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
