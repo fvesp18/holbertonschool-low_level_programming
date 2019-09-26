1a3849ae862670765e01d98b9ce0392c6f05dae9
fvesp18 <fespholberton@gmail.com>
2019-09-26 17:42:11 +0000

    Print to 100 with divisors of 3 printing Fizz, divisors 5 printing Buzz and divisors of both printing both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print fizz every 3, buzz every 5 and fizzbuzz for both
+ * Return: void
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if ((num % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		num++;
+	}
+	putchar(10);
+	return (0);
+}
