554e987bdd78a596edb587d25e5d766d9aea0a8a
fvesp18 <fespholberton@gmail.com>
2019-10-01 14:14:02 +0000

    Program that will flip int a value with int b and then again reverse

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap the values of int a and int b
+ * @a: first value
+ * @b: second value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
