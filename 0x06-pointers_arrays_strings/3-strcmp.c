fab0e62165b4f9e8a958f0a9ce0a1e0a72573e3e
fvesp18 <fespholberton@gmail.com>
2019-10-04 01:47:59 +0000

    Add comments on parameters and funtion

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,26 @@
 #include "holberton.h"
 
+/**
+ * _strcmp - compares strings
+ * @s1: string one
+ * @s2: string two
+ * Return: -15, 15, 0
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 
 	if (*s1 < *s2)
 	{
-		return(-15);
+		return (-15);
 	}
 	if (*s2 < *s1)
 	{
-		return(15);
+		return (15);
 	}
 	if (*s1 == *s1)
 	{
-		return(0);
+		return (0);
 	}
 
 }
