a16c8f809c454688e2b4357fdc911104701abf02
fvesp18 <fespholberton@gmail.com>
2019-10-04 03:31:27 +0000

    Compare two string, return -15, 15, 0

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,12 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	if (*s1 < *s2)
+	while (*s1 && (*s1 == *s2))
 	{
-		return (-15);
+		s1++;
+		s2++;
 	}
-	if (*s2 < *s1)
-	{
-		return (15);
-	}
-	if (*s1 == *s1)
-	{
-		return (0);
-	}
+
+	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 
 }
