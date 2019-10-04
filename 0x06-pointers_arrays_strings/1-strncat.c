a067ea635b6ecc18c92c2b41a5b96e9f9461056a
fvesp18 <fespholberton@gmail.com>
2019-10-04 00:55:47 +0000

    Program adds string src to string dest while not adding null

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: non null termination
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	char *out = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (n--)
+	{
+		if (!(*dest++ = *src++))
+		return (out);
+	}
+
+	*dest = 0;
+
+	return (out);
+}
