14d5c4624c82773cfc62b2266ec98f6eddc02ddf
fvesp18 <fespholberton@gmail.com>
2019-10-04 03:09:46 +0000

    Add comments for betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,17 @@
 #include "holberton.h"
 
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: size of array
+ * Return: void
+ */
+
 void reverse_array(int *a, int n)
 {
 	int i = 0;
 	int buffer;
+
 	n = n - 1;
 
 	while (i < n)
