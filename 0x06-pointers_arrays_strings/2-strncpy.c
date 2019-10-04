6bc406f51a27e5e95d687982b5391514f64d5d01
fvesp18 <fespholberton@gmail.com>
2019-10-04 01:22:41 +0000

    Add comments to parameters

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,11 @@
 
 /**
  * _strncpy - copy string src to string dest
+ * @dest: destination string
+ * @src: source string
+ * @fir: buffer
+ * @n: pointer location
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
