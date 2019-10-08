f4a277a9c2f36089a9e50e54018ec0c570c8d536
fvesp18 <fespholberton@gmail.com>
2019-10-08 19:57:07 +0000

    Program sets value of pointer s to to

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - point pointer to pointer
+ * @s: pointer pointing to pointer
+ * @to: pointer pointed to by pointer s
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+
+}
