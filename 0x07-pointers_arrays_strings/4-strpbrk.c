252bd7427cbaac6a3539d5fba04a225e1e01b0b7
fvesp18 <fespholberton@gmail.com>
2019-10-09 03:59:49 +0000

    Create loops to run through src string and find instances

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,6 +3,24 @@
 char *_strpbrk(char *s, char *accept)
 {
 
-	return (s);
+	int i;
+	int ii;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; accept[ii] != '\0'; ii++)
+		{
+
+			if (accept[ii] != s[i])
+			{
+
+				return (s);
+
+			}
+			if (s[i] == 0)
+				break;
+		}
+
+	}
+	return (0);
 }
