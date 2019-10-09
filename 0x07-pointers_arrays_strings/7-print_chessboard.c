ac11e8d0886867c3bdb4df2b172f53393810a5d6
fvesp18 <fespholberton@gmail.com>
2019-10-09 03:38:42 +0000

    program prints chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar (a[row][col]);
+		}
+		_putchar ('\n');
+	}
+
+}
