a1d88b2f0ef00f6783bdfa7cc6c1bfebe8c630b2
fvesp18 <fespholberton@gmail.com>
2019-10-10 03:59:13 +0000

    Init commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,32 @@
 #include "holberton.h"
 
 /**
+ * helper - helper
+ * @n: number
+ * @div: divisor
+ * Return: prime
+ */
+
+int helper(int n, int div)
+{
+	if (n <= 2)
+	{
+		return (n == 2);
+	}
+
+       	if (n % div ==  0)
+       	{
+       		return (0);
+       	}
+
+	if (div * div > n)
+	{
+		return (1);
+	}
+	return (helper(n, div + 1));
+}
+
+/**
  * is_prime_number - determines if a number is prime or not\
  * @n: number at hand
  * Return: 1, if prime, 0 if not
@@ -8,22 +34,8 @@
 
 int is_prime_number(int n)
 {
-	int div = n;
+	if (n == 0)
+		return (0);
 
-	if (div == 1)
-	{
-		return (1);
-	}
-
-	else
-	{
-		if (n % div == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return (is_prime_number(n));
-		}
-	}
+       	return (helper(n, 2));
 }
