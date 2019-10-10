dc7ddc69f63684dc53a57c88823e5e18a761aff9
fvesp18 <fespholberton@gmail.com>
2019-10-10 03:32:55 +0000

    Init commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - square root int n
+ * @n: base int
+ * Return: -1
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	int sind = 1;
+	int eind = x;
+	int temp = 0;
+
+	_sqrt_recursion(n/2);
+	if (_sqrt_recursion != temp)
+		_sqrt_recursion ((n/temp + temp) / 2)
+
+_sqrtr
+}
