e8359f91db1628b93b7400edcaceba4fb1a9bbc1
fvesp18 <fespholberton@gmail.com>
2019-10-11 22:17:43 +0000

    Adds command line arguments if real numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{
+
+	int arg1;
+	int arg2;
+	int sum = 0;
+
+	if (argc < 0)
+	{
+		printf("0\n");
+	}
+
+       	for (arg1 = 1; arg1 < argc; arg1++)
+       	{
+       	       	sum += atoi(argv[arg1]);
+
+		for (arg2 = 0; argv[arg1][arg2] != '\0'; arg2++)
+       		{
+			if (!(isdigit (argv[arg1][arg2])))
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+       	}
+       	printf("%d\n", sum);
+	return (0);
+}
