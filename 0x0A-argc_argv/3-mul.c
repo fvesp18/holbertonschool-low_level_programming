e3e9aaf7be0f4536373c2a3aa9c83a07818a9caf
fvesp18 <fespholberton@gmail.com>
2019-10-13 23:00:53 +0000

    Add comments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - multplies arguments in command line
+ * @argc: multiplier counter
+ * @argv: multiplier vector
+ * @prod: product
+ * Return: 0, upon success
+ */
+
 int main(int argc, char **argv)
 {
-	int ind = 1;
 	int prod = 1;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-		for (; ind < argc; ind++)
-		{
-			prod *= atoi(argv[ind]);
-		}
+		prod = atoi(argv[argc + 1]) * atoi(argv[argc + 2]);
 		printf("%d\n", prod);
 	}
 
 	if (argc == 1)
 		printf("Error\n");
 
-       	return (1);
+       	return (0);
 
 }
