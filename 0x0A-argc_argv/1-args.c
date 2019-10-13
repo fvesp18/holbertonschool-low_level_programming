3fe678e202dd226cb78e5a59d9a8389d0ad05673
fvesp18 <fespholberton@gmail.com>
2019-10-13 22:47:39 +0000

    Fix for betty commenting

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints number of arguments passed
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0, upon success
+ */
+
 int main(int argc, char **argv)
 {
 	(void) (**argv);
