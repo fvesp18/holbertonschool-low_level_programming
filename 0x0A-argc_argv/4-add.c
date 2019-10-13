9648f5440f055a6d76b3696d6d6003e451727283
fvesp18 <fespholberton@gmail.com>
2019-10-13 23:56:51 +0000

    Fix commenting and spacing

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,9 +6,6 @@
  * main - add all positive nums
  * @argc: arguments counter
  * @argv: arugments vector
- * @arg1: iterator for arg1
- * @arg2: iterator for arg2
- * @sum: summation fo arg1 & arg2
  * Return: 0, upon success
  */
 
@@ -24,19 +21,19 @@ int main(int argc, char **argv)
 		printf("0\n");
 	}
 
-       	for (arg1 = 1; arg1 < argc; arg1++)
-       	{
-       	       	sum += atoi(argv[arg1]);
+	for (arg1 = 1; arg1 < argc; arg1++)
+	{
+		sum += atoi(argv[arg1]);
 
 		for (arg2 = 0; argv[arg1][arg2] != '\0'; arg2++)
-       		{
-			if (!(isdigit (argv[arg1][arg2])))
+		{
+			if (!(isdigit(argv[arg1][arg2])))
 			{
 				printf("Error\n");
 				return (0);
 			}
 		}
-       	}
-       	printf("%d\n", sum);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
