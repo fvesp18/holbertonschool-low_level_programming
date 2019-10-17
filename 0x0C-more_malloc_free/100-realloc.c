4fcabfdce9f2531a28e8bda74858cd5bc02b38c9
fvesp18 <fespholberton@gmail.com>
2019-10-17 22:36:22 +0000

    Initial commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory and free
+ * @ptr: pointer
+ * @old_size: old malloc
+ * @new_size: new malloc, reallocation
+ * Return: NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *rere;
+
+	if (!ptr)
+		return (NULL);
+
+	if (old_size < new_size)
+		return (NULL);
+
+	return (rere);
+}
