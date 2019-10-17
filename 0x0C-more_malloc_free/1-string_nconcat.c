b0c97a22936f3ca5e856766524261965cc9d9b8e
fvesp18 <fespholberton@gmail.com>
2019-10-17 22:05:22 +0000

    Fix betty formatting

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,6 +6,7 @@
  * string_nconcat - concatenates two strings
  * @s1: string one
  * @s2: string two
+ * @n: size to allocate for s2
  * Return: ptr
  */
 
@@ -13,39 +14,41 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	/* declare variables */
 	unsigned int len1 = 0, len2 = 0, count = 0, begs = 0;
-        char *cat;
+	char *cat;
 
-        /* check for empty strings */
+	/* check for empty strings */
 	if (s1 == NULL)
 		s1 = "\0";
 	if (s2 == NULL)
-                s2 = "\0";
+		s2 = "\0";
 
-        /* find length of strings */
-        for (count = 0; s1[count] != '\0'; count++)
-                len1++;
+	/* find length of strings */
+	for (count = 0; s1[count] != '\0'; count++)
+		len1++;
 
-        for (count = 0; s2[count] != '\0'; count++)
-                len2++;
+	for (count = 0; s2[count] != '\0'; count++)
+		len2++;
 
 	if (n >= len2)
 		n = len2;
 
-        /* allocate memory for concatenated string */
-        cat = malloc((len1 + n + 1) * sizeof(char));
+	/* allocate memory for concatenated string */
+	cat = malloc((len1 + n + 1) * sizeof(char));
 
-        /* malloc fail safe */
-        if (cat == NULL)
-                return (NULL);
+	/* malloc fail safe */
+	if (cat == NULL)
+		return (NULL);
 
-        /* duplicate s1 to cat */
-        for (count = 0; count < len1; count++)
-                cat[count] = s1[count];
+	/* duplicate s1 to cat */
+	for (count = 0; count < len1; count++)
+		cat[count] = s1[count];
 
-        /* add s2 to end of s1 to cat */
-        for (begs = 0; begs <= n; begs++, count++)
-                cat[count] = s2[begs];
+	/* add s2 to end of s1 to cat */
+	for (begs = 0; begs <= n; begs++, count++)
+		cat[count] = s2[begs];
 
-        /* return pointer */
-        return (cat);
+	cat[count] = '\0';
+
+	/* return pointer */
+	return (cat);
 }
