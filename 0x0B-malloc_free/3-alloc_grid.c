bfad7355b1e0627095ded710d371b85c6f3c7a54
fvesp18 <fespholberton@gmail.com>
2019-10-17 18:50:55 +0000

    Change pointer notation to array notation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,33 @@
 
 int **alloc_grid(int width, int height)
 {
-	int row, col, i;
+	int row, col;
        	int **grid;
 
-	if (width <= 0 || height <= 0)
+	if (width < 1 || height < 1)
 		return (NULL);
 
-	grid = malloc(sizeof(int*) * height);
+	grid = malloc( height * sizeof(int*));
 	if (grid == NULL)
 		return (NULL);
 
 	for (row = 0; row < height; row++)
 	{
-		*(grid + row) = malloc(sizeof(int) * width);
-		if (*(grid + row) == NULL)
-			return (NULL);
-		for (i = 0; i < row; i++)
+		grid[row] = malloc(width * sizeof(int));
+
+		if (grid[row] == NULL)
 		{
-			free(*(grid + i));
-			free(grid);
-			return (NULL);
+		for (col = 0; col < row; col++)
+			free(grid[col]);
+
+       		free(grid);
+       		return (NULL);
 		}
 	}
-	for (row = 0; row < height; row++)
-		for (col = 0; col < width; col++)
-			grid[row][col] = 0;
 
+       	for (col = 0; col < width; col++)
+	{
+			grid[row][col] = 0;
+	}
 	return (grid);
 }
