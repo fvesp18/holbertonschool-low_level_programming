9c4610270aa9f5ffa6b7eb54d58380476d076f6b
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-21 22:29:01 +0000

    Add description of parameters

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints name of file
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0, upon success
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) (argc);
 	(void) (argv);
 
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
