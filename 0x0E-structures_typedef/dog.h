3871fc77a3f024e04ed16df77020e04af24cab4b
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-22 00:47:08 +0000

    Add description of struct dog, name parameters

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - holds info for dog.name, dog.age, and dog.owner
+ * @name: name
+ * @owner: owner
+ * @age: age
+ */
+
 struct dog
 {
 	char *name;
