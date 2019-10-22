23e45eb9923e64c56348972e46a4a87b89f6a384
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-22 17:19:40 +0000

    change printf statements if null to nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		d->owner = "(nil)";
 	if (d == NULL)
 		return;
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
