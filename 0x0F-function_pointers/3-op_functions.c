b86270fcb8f68fe8b3f92133d13e7806484937ef
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-24 22:58:50 +0000

    Operator functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds integers
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts b from a
+ * @a: first
+ * @b: second
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a with b
+ * @a: first
+ * @b: second
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide b from a
+ * @a: numerator
+ * @b: denominator
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a modulo b
+ * @a: first
+ * @b: second
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
