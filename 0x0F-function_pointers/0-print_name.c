a183226c765006c47204fd139982510e4d7b1afa
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-24 19:56:00 +0000

    Add statement that will print name using function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,15 @@
 #include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: none
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (f && name)
+		f(name);
+}
