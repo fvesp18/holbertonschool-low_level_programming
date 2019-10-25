0d76a7f0d162dd7a3987df33fa16c7af8caad52c
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-25 00:24:19 +0000

    Add commenting

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,6 +8,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	int i = 0;
 	op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
@@ -16,15 +17,13 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i = 0;
-
 	while (ops[i].op != NULL)
 	{
-		if ((*ops[i].op == *s) && !(s[1]))
+		if ((*ops[i].op == *s) && !(s + 1))
 		{
-			return (op[1].f);
+			return (ops[i].f);
 		}
-		i++
+		i++;
 	}
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,26 +10,30 @@
 int main(int argc, char *argv[])
 {
 	int (*ptr)(int, int);
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
 
+	ptr = get_op_func(argv[2]);
+	
 	if (argc != 4)
 	{
 		printf ("Error\n");
+		printf ("ARGS");
 		exit (98);
 	}
-
-	ptr = get_op_func(argv[2]);
-
 	if (ptr == NULL)
 	{
 		printf ("Error\n");
+		printf ("PTR");
 		exit (99);
 	}
-	if ((*argv[3] == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+	if ((num2 == 0) && ((*argv[2] == '/') || (*argv[2] == '%')))
 	{
 		printf("Error\n");
+		printf ("MOD");
 		exit (100);
 	}
 
-	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", ptr(num1, num2));
 	return (0);
 }
