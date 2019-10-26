49e8fc734e83ef7ec1651f7cb72eb85821cc27b3
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-26 17:06:39 +0000

    Prints list using MACROS of type va

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: separator
+ * @n: number of integers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int arg;
+	unsigned int s = 0;
+	va_list args;
+	
+	va_start (args, n);
+
+	for (s = 0; s < n; s++)
+	{
+		arg = va_arg (args, int);
+		printf("%d", arg);
+		va_end (args);
+		if (separator != NULL && s < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
