9d23d6c721e35b62d6e48a6d5fa5c8399ec345f8
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-26 17:07:08 +0000

    Prints strings using the va MACROS

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separates strings
+ * @n: argument counter
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int cnt;
+	char *str;
+	va_list strings;
+
+	va_start (strings, n);
+	for (cnt = 0; cnt < n; cnt++)
+	{
+		str = va_arg (strings, char *);
+		if (str == NULL)
+			printf("(nil)");
+		if (separator != NULL && cnt < n - 1)
+			printf("%s%s", str, separator);
+		else
+		printf("%s", str);
+	}
+	va_end (strings);
+	printf("\n");
+}
