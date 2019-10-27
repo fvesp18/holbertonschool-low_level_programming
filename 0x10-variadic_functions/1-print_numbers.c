2e2c54af87ce069910ca648341231b70093b75fb
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-27 20:38:31 +0000

    Delete space between function and open parenthesis, line 15

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,13 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int arg;
 	unsigned int s = 0;
 	va_list args;
-	va_start (args, n);
+	va_start(args, n);
 
 	for (s = 0; s < n; s++)
 	{
 		arg = va_arg (args, int);
 		printf("%d", arg);
-		va_end (args);
+		va_end(args);
 		if (separator != NULL && s < n - 1)
 			printf("%s", separator);
 	}
