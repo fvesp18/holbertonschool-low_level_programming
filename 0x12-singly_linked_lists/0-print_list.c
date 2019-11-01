79265399b8156e1c8a3c954030af981b2cb3d5f4
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-01 16:03:21 +0000

    Initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: list of elements to be printed
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int b = 0;
+	
+	while (h != NULL)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		h = h->next;
+		b++;
+	}
+	return (b);
+}
