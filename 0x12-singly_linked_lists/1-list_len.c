d0c511c738949d2f3e329b2ac4cef9bc59c3db79
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-01 17:10:02 +0000

    Change unsigned int to regular int, set to 0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,14 @@
 #include "lists.h"
-
 /**
  * list_len - returns number of elements in a list
  * @h: list to be counted
  * Return: numbers of elements
  */
-
 size_t list_len(const list_t *h)
 {
-	unsigned int cnt;
+	int cnt = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		h = h->next;
 		cnt++;
