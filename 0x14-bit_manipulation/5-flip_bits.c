ed55df73148cfec6c0d6469fa302ca58aee38b31
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-07 23:00:46 +0000

    initial commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * flip_bits - does the flip thing
+ * @n: integer to be converted
+ * @m: integert to be converted
+ * Return: some unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	if (n)
+		return (n);
+	if (m)
+		return (m);
+	return (0);
+}
