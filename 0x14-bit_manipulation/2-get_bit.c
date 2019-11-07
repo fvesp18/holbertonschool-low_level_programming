32ede75061dba3a145a3fc7a479f4d228bc3dd7b
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-07 22:12:23 +0000

    Returns value of bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * get_bit - returns value of a bit at a given index
+ * @n: int at hand
+ * @index: index
+ * Return: value of bit at index, or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = (n >> index) & 1;
+	if (mask >= 1)
+		return (mask);
+	if (index > sizeof(n) * 8)
+		return (-1);
+	if (mask & n)
+		return (1);
+
+	return (0);
+}
