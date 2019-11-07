f810729a08979285c63ef8f42194298a1ad36190
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-07 23:06:15 +0000

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts to binary
+ * @b: binary string
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	if (*b)
+		return (1);
+	else 
+		return (0);
+}
