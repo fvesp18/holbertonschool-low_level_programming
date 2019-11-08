8f3ab572640ba37accf9ccd639a01f38813cb3f8
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-08 19:29:36 +0000

    View history

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,8 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (*b)
-		return (1);
-	else
+	unsigned int conv = 0;
+	int i = 0;
+
+	if (!b)
 		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] == '1')
+			conv = (conv << 1) | 1;
+		else if (b[i] == '0')
+			conv = (conv << 1);
+		else
+			return (0);
+	}
+	return (conv);
 }
