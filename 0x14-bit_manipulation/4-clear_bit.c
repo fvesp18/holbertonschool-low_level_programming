a3c8529f5c7886ce064297ca63dd2d7c5bd7a0a5
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-08 19:28:37 +0000

    Initialize var in code

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,8 +7,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask = 1 << index;
+	unsigned long int mask;
 
+	mask = (1 << index);
 	if (sizeof(n) * 8)
 	{
 		*n = (*n & ~mask);
