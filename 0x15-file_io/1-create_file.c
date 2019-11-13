c5e53ad392e820a3ec8189559e21765546bab809
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-13 00:29:15 +0000

    Initial file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * create_file - creates file
+ * @filename: file of name to be created
+ * @text_content: data inside said file
+ * Return: -1, 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+	if (!text_content)
+		return (-1);
+
+	return (0);
+}
