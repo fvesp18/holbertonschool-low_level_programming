826e8439c99d282df6192baceab437eeea8e616d
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-13 18:06:34 +0000

    Reads textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
-	if (letters)
+	int fd;
+	int count = 0;
+	ssize_t writeme;
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer || !filename)
 		return (0);
 
-	return (letters);
+	fd = open(filename, O_RDONLY);
+		if (fd == -1)
+			return (0);
+
+	count = read(fd, buffer, letters);
+		if (count == -1)
+			return (0);
+
+	writeme = write(STDOUT_FILENO, buffer, count);
+		if (writeme == -1)
+			return (0);
+	close(fd);
+	free(buffer);
+
+	return (count);
 }
