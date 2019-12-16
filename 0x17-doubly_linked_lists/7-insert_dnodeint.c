d5c38f720e82106657d1eef77ef727688e3082f8
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-16 02:45:40 +0000

    compare i to idx, not idx - 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	for (i = 2; i <= idx - 1; i++)
+	for (i = 2; i <= idx; i++)
 	{
 		ptr = ptr->next;
 	}
