6117b8eb61a593f5fb947210254bc1cccd2958f6
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-16 04:15:30 +0000

    deletes node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_dnodeint_at_index = deletes node at index
+ * delete_dnodeint_at_index - deletes node at index
  * @head: head of list
  * @index: point of deletion
  * Return: 1, if succeed. else, -1
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	buff->prev = NULL;
 	buff->next = NULL;
 
-	if (!*head)
+	if (*head == NULL)
 		return (1);
 	if (index == 0)
 	{
@@ -26,24 +26,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(buff);
 		return (1);
 	}
+	for (cmp = 0; buff != NULL && cmp < index; cmp++)
+		buff->next = (*head)->next;
+
 	if (buff == NULL || buff->next == NULL)
 		return (1);
+	next = buff->next->next;
 
-	if (*head == buff)
+/*	if (*head == buff)
 		*head = buff->next;
 	if (buff->next)
 		buff->next->prev = buff->prev;
 	if (buff->prev)
 		buff->prev->next = buff->next;
-	next = buff->next->next;
+*/
 	free(buff->next);
 	buff->next = next;
 
 	return (1);
-
-	for (cmp = 0; buff != NULL && cmp < index - 1; cmp++)
-	{
-		buff->next = (*head)->next;
-	}
-	buff->next = NULL;
 }
