5b4c59c388573b1e01f591e88168f276a812f970
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-20 01:21:50 +0000

    initial commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to newly created hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *root;
+
+	if (size < 1)
+		return (NULL);
+
+	root = malloc(sizeof(hash_table_t));
+
+	if (root == NULL)
+		return (NULL);
+
+	root->array = malloc(size * sizeof(hash_node_t));
+
+	if (root->array == NULL)
+		return (NULL);
+
+	memset(root->array, 0, size);
+
+	root->size = size;
+
+	return (root);
+}
