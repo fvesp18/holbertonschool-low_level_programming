32d415294f6c53ae46c14fc8bc1b3cfb40fc1fde
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-22 02:42:59 +0000

    [NEW] CRY

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" LETS GET ITTTTTTT """
+
+
+def island_perimeter(grid):
+    count = 0
+    length_list = len(grid)
+    length_ele = len(grid[0])
+    for i in range(length_list):
+        for j in range(length_ele):
+            if grid[i][j] == 1:
+                if i + 1 < length_list and grid[i + 1][j] == 0:
+                    count += 1
+                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                    count += 1
+                if i - 1 >= 0  and grid[i - 1][j] == 0:
+                    count += 1
+                if j + 1 < length_ele and grid[i][j + 1] == 0:
+                    count += 1
+    return count
