d1b2dcfb353aaf5b407e96426bcc93944c7bcfbd
fvesp18 <1081@holbertonschool.com>
2020-06-04 19:44:23 +0000

    [UPDATE] Change comp data type to unsigned int

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,11 +14,11 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t comp = 0;
+	unsigned int comp = 0;
 
-	for ( ; comp <= size ; comp++)
+	for ( ; comp < size ; comp++)
 	{
-		printf("Value checked array[%lu] = [%d]\n",
+		printf("Value checked array[%d] = [%d]\n",
 				comp, array[comp]);
 		if (array[comp] == value)
 			return (comp);
