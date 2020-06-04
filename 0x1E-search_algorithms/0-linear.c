de7436d1aca6842d54c2f2d16fd5c3c36dbfaa5f
fvesp18 <1081@holbertonschool.com>
2020-06-04 19:33:50 +0000

    [UPDATE] Update for betty compliance

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,15 +3,25 @@
 #include "search_algos.h"
 
 
+/**
+ * linear_search - algorithm
+ * @array: data to be processed
+ * @size: size of array
+ * @value: value found at array[comp]
+ * Return: comp, or -1 on failure
+ */
+
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t comp = 0;
-	for (; comp <= size; comp++)
+
+	for ( ; comp <= size ; comp++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", 
+		printf("Value checked array[%lu] = [%d]\n",
 				comp, array[comp]);
 		if (array[comp] == value)
-			return(comp);
+			return (comp);
 	}
-	return(-1);
+	return (-1);
 }
